e8ae360864ed823cbe7fefdd0a2804597025cb2b
Myron Holmes <1dequanh@gmail.com>
2023-10-03 23:29:55 -0400

    updated

---
+++ index.js
@@ -0,0 +1,10 @@
+let github = document.getElementById("gh")
+let linkedin = document.getElementById("li")
+
+function newTabGH(){
+    window.open('https://github.com/MyronHolmes', '_blank')
+}
+
+function newTabLI(){
+    window.open('https://www.linkedin.com/in/myron-holmes/', '_blank')
+}
