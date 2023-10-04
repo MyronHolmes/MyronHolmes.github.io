1fa3864acad2d9733e64c46afdb695325f2c5942
Myron Holmes <1dequanh@gmail.com>
2023-10-04 13:14:11 -0400

    completed portfolio

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-let github = document.getElementById("gh")
-let linkedin = document.getElementById("li")
-
-function newTabGH(){
-    window.open('https://github.com/MyronHolmes', '_blank')
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
+function newTabRes(){
+  window.open('https://docs.google.com/document/d/1PZ965xA8bNEQguxY3xk85X_gQKZZ3v5JxX1Zq72HRCA/edit?usp=sharing', '_blank')
+}
 function newTabLI(){
-    window.open('https://www.linkedin.com/in/myron-holmes/', '_blank')
+  window.open('https://www.linkedin.com/in/myron-holmes/', '_blank')
+}
+function newTabGH(){
+  window.open('https://github.com/MyronHolmes', '_blank')
+}
+function githubCBM(){
+  window.open('https://github.com/MyronHolmes/Foundations-Capston', '_blank')
+}
+function demoCBM(){
+  window.open('https://www.youtube.com/watch?v=qUBWoKF-Kds', '_blank')
+}
+function githubPP(){
+  window.open('https://github.com/MyronHolmes/Java-Capstone-Project', '_blank')
+}
+function demoPP(){
+  window.open('https://www.youtube.com/watch?v=VdTFwFF-Mds', '_blank')
 }
