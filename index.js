cbef652e963cee0964610bfaf58dcf9cf7ffb7a9
Myron Holmes <1dequanh@gmail.com>
2025-09-15 19:44:09 -0400

    Navbar was added and is functional. Mobile incudes a hamburger menu. Hovering over some items will either change colors or increase in size. Both the Hero and About Me sections are now complete. Changes where made to the png, renamed the dev cert, switched the icons and added all to showcase on the portfolio.

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+  console.log("HIT")
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menu-btn");
+  const menu = document.getElementById("menu");
 
-function newTabRes(){
-  window.open('https://docs.google.com/document/d/1PZ965xA8bNEQguxY3xk85X_gQKZZ3v5JxX1Zq72HRCA/edit?usp=sharing', '_blank')
+  menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("open");
+
+    if (menu.classList.contains("-translate-y-full")) {
+      menu.classList.remove("-translate-y-full", "opacity-0");
+      menu.classList.add("translate-y-0", "opacity-100", "flex");
+    } else {
+      menu.classList.add("-translate-y-full", "opacity-0");
+      menu.classList.remove("translate-y-0", "opacity-100", "flex");
+    }
+  });
+
+  // Close menu on link click
+  menu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.add("-translate-y-full", "opacity-0");
+      menu.classList.remove("translate-y-0", "opacity-100", "flex");
+      menuBtn.classList.remove("open");
+    });
+  });
+});
+
+
+function newTabRes() {
+  window.open(
+    "https://docs.google.com/document/d/1PZ965xA8bNEQguxY3xk85X_gQKZZ3v5JxX1Zq72HRCA/edit?usp=sharing",
+    "_blank"
+  );
 }
-function newTabLI(){
-  window.open('https://www.linkedin.com/in/myron-holmes/', '_blank')
+function newTabLI() {
+  window.open("https://www.linkedin.com/in/myron-holmes/", "_blank");
 }
-function newTabGH(){
-  window.open('https://github.com/MyronHolmes', '_blank')
+function newTabGH() {
+  window.open("https://github.com/MyronHolmes", "_blank");
 }
-function githubCBM(){
-  window.open('https://github.com/MyronHolmes/Foundations-Capston', '_blank')
+function githubCBM() {
+  window.open("https://github.com/MyronHolmes/Foundations-Capston", "_blank");
 }
-function demoCBM(){
-  window.open('https://www.youtube.com/watch?v=qUBWoKF-Kds', '_blank')
+function demoCBM() {
+  window.open("https://www.youtube.com/watch?v=qUBWoKF-Kds", "_blank");
 }
-function githubPP(){
-  window.open('https://github.com/MyronHolmes/Java-Capstone-Project', '_blank')
+function githubPP() {
+  window.open("https://github.com/MyronHolmes/Java-Capstone-Project", "_blank");
 }
-function demoPP(){
-  window.open('https://www.youtube.com/watch?v=VdTFwFF-Mds', '_blank')
+function demoPP() {
+  window.open("https://www.youtube.com/watch?v=VdTFwFF-Mds", "_blank");
 }
-function githubDW(){
-  window.open('https://github.com/MyronHolmes/dental-website', '_blank')
+function githubDW() {
+  window.open("https://github.com/MyronHolmes/dental-website", "_blank");
 }
-function demoDW(){
-  window.open('https://www.youtube.com/watch?v=lJkTsjZb12w', '_blank')
+function demoDW() {
+  window.open("https://www.youtube.com/watch?v=lJkTsjZb12w", "_blank");
 }
